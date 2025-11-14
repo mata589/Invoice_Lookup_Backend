4e7d89e4a90c74c917dfdad9b9b75ef99c1e26a7
KIZITO RICHARD <mata589kizitomata@gmail.com>
2025-11-14 12:04:05 +0300

    Updates

--- src/main/java/com/example/invoice_lookup/config/SecurityConfig.java
+++ src/main/java/com/example/invoice_lookup/config/SecurityConfig.java
@@ -61,7 +61,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
                 "http://localhost:8080",
-                "http://127.0.0.1:8080"
+                "http://127.0.0.1:8080",
+                "http://10.9.0.130:3000"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
